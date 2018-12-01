22726e24ad207f5a9dbb90d6520ef6da68b0e169
Colin Danforth <cdanforth@loyalty.com>
2018-12-01 17:05:23 -0500

    committing additional assets missed on last commit

---
+++ src/components/Header.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Logo from './500px_logo_detail.png'
+import ReactResizeDetector from 'react-resize-detector';
+
+const headerStyle={
+  position: 'fixed',
+  top: '0px',
+  left: '0px',
+  width: '100%',
+  hieght: 'auto',
+  backgroundColor: '#FFF'
+}
+
+const logoStyle = {
+  maxWidth: '100px',
+  width: 'auto',
+  height: 'auto',
+  margin: '0.8%',
+}
+
+class Header extends React.Component{
+  render(){
+    return(
+      <div style={headerStyle}>
+        <a href='/'>
+          <img style={logoStyle} src={Logo} alt='logo'/>
+        </a>
+        <ReactResizeDetector handleWidth handleHeight onResize={this.props.onResize} />
+      </div>
+    )
+  }
+}
+
+export default Header
+
+Header.propTypes={
+  onResize: PropTypes.func,
+}
