e40037d935c6b79ff09748525f142d98e6b4e1b4
Colin Danforth <cdanforth@loyalty.com>
2018-12-01 10:19:09 -0500

    updates to include basic page information in README.md

--- src/components/ImagePage.js
+++ src/components/ImagePage.js
@@ -1,25 +1,45 @@
 import React from 'react'
 import client from './client'
 
+const popularImages = async (page) => {
+  return await client.fetchPopular(page)
+}
+
+const imageHeight = {
+  maxHeight: '280px',
+  width: 'auto',
+  height: 'auto'
+}
+
 class ImagePage extends React.Component{
   constructor(){
     super()
-    this.state={
+    this.state= {
       images: []
     }
   }
 
-  componentWillMount(){
-    const fetchPopular = client.fetchPopular()
-    this.setState={
-      images: fetchPopular
-    }
+  componentDidMount(){
+    popularImages(1)
+      .then(response => {
+        this.setState({
+          images: [response]
+        })
+      })
   }
 
   render(){
     return(
       <div>
-        <p>{this.state.images}</p>
+        {this.state.images.length > 0 ?
+          (
+            <div>
+              {this.state.images[0].photos.map((object, i) => <img style={imageHeight} key={i} src={object.images[0].url} alt="test"/>)}
+            </div>
+          )
+          :
+          (<p>but why</p>)
+        }
       </div>
     )
   }
--- src/components/client.js
+++ src/components/client.js
@@ -1,13 +1,33 @@
-import fs from 'fs'
+import key from '../consumer'
 
-const fetchPopular = async() => {
-  const key = fs.readFile('../../../key.js', 'utf8', function(err, contents) {
-    console.log(contents);
-  });
+const fetchPopular = async(currentPage) => {
 
-  return await fetch(`https://api.500px.com/v1/photos?feature=popular&consumer_key=${key}`, (result) => {
-    return JSON.parse(result)
+  return await fetch(`https://api.500px.com/v1/photos?feature=popular&image_size=20&page=${currentPage}&consumer_key=${key.key}`,
+    {
+      headers: new Headers({
+        'Content-Type': 'application/json, UTF-8',
+      })
+    }).then(response => {
+      const reader = response.body.getReader()
+    return new ReadableStream({
+      start(controller) {
+        return pump()
+        function pump() {
+          return reader.read().then(({ done, value }) => {
+            // When no more data needs to be consumed, close the stream
+            if (done) {
+              controller.close()
+              return;
+            }
+            // Enqueue the next data chunk into our target stream
+            controller.enqueue(value)
+            return pump()
+          });
+        }
+      }
+    })
   })
+    .then(stream => new Response(stream))
+    .then(response => response.json())
 }
-
 export default {fetchPopular}
